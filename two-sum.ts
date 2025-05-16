21eb335a7e197922d9e4f23eafb839239cc450c4
lucasthalless <67612669+lucasthalless@users.noreply.github.com>
2025-05-16 16:34:04 -0300

    submission -> two-sum.ts

---
+++ two-sum.ts
@@ -0,0 +1,16 @@
+function twoSum(nums: number[], target: number): number[] {
+    let currIndex = 0;
+    while (currIndex < nums.length) {
+        for (let numsIndex = 0; numsIndex < nums.length; numsIndex++) {
+            if (numsIndex !== currIndex) {
+                if  (nums[numsIndex] + nums[currIndex] === target) {
+                    return [numsIndex, currIndex].sort();
+                }
+            }
+            
+            if (numsIndex === nums.length - 1) {
+                currIndex++;
+            }
+        }
+    }
+};
