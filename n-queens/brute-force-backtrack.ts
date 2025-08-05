af971df0aa7b4f0590af19d5a3d67fe45b927cca
lucasthalless <67612669+lucasthalless@users.noreply.github.com>
2025-08-05 20:51:38 -0300

    update brute-force-backtrack.ts

--- n-queens/brute-force-backtrack.ts
+++ n-queens/brute-force-backtrack.ts
@@ -1,20 +1,44 @@
 function solveNQueens(n: number): string[][] {
-    const cols = new Set([])
-    const positiveDiagonals = new Set([]) // (rows + columns)
-    const negativeDiagonals = new Set([])// (rows - columns)
+    const cols = new Set<number>([])
+    const positiveDiagonals = new Set<number>([]) // (rows + columns)
+    const negativeDiagonals = new Set<number>([])// (rows - columns)
 
     const resultBoards: string[][] = []
+    let newBoard: string[] = []
 
-    for(r = 0; r < n; r++) {
+    for(let r = 0; r < n; r++) {
         if (r == n) {
             return resultBoards
         }
-
-        backtrack()
+        
+        const filledRow = fillRowBacktrack(r, 0, n, cols, positiveDiagonals, negativeDiagonals, resultBoards);
+        newBoard.push(filledRow);
+        
     }
 
-    function backtrack(row: number, col: number, rows: Set<number>, cols: Set<number>, resultBoards: [][]) {
+    console.log(newBoard)
 
+    function fillRowBacktrack(row: number, col: number, rowLength: number, cols: Set<number>, positiveDiagonals: Set<number>, negativeDiagonals: Set<number>, resultBoards: string[][]): string {
+        const isInDiagonal = positiveDiagonals.has(row + col) || negativeDiagonals.has(row - col)
+
+        if (!cols.has(col) && !isInDiagonal) {
+            let filledRow = ""
+            for (let i = 0; i < rowLength; i++) {
+                if (i === col) {
+                    filledRow += "Q"
+                } else {
+                    filledRow += "."
+                }
+            }
+
+            cols.add(col)
+            positiveDiagonals.add((row + col))
+            negativeDiagonals.add((row - col))
+            return filledRow
+        }
+
+        return fillRowBacktrack(row, col + 1, rowLength, cols, positiveDiagonals, negativeDiagonals, resultBoards)
     }
 
+    return resultBoards;
 };
